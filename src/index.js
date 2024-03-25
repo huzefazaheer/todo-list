6cb291c3b3878b76c4a0a3fc6d2803920fb53caf
Huzefa <huzefazaheer1+git@gmail.com>
2024-03-25 21:02:37 +0500

    Seperated to do, doing and done tasks

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ tasks = [
   new task("Eat", "Get fat", "Today"),
   new task("Sleep", "Zzzzzz", "Today"),
   new task("Grind", "They don't know me son", "Today"),
-  new task("Eat", "In my mind, I am always the best", "Today"),
+  new task("Repeat", "In my mind, I am always the best", "Today"),
 ];
 
 home = new project("Home", tasks);
--- src/util/displayproject.js
+++ src/util/displayproject.js
@@ -1,11 +1,33 @@
 const projectdiv = document.querySelector(".project");
 
 export function displayProject(project) {
-  displayTask(project.taskList.Todo);
+  const tododiv = document.createElement("div");
+  const headingTodo = document.createElement("h3");
+  headingTodo.innerText = "To Do";
+  tododiv.appendChild(headingTodo);
+  projectdiv.appendChild(tododiv);
+  displayTask(project.taskList.Todo, tododiv);
+
+  const doingdiv = document.createElement("div");
+  const headingDoing = document.createElement("h3");
+  headingDoing.innerText = "Doing";
+  doingdiv.appendChild(headingDoing);
+  projectdiv.appendChild(doingdiv);
+  displayTask(project.taskList.Doing, doingdiv);
+
+  const donediv = document.createElement("div");
+  const headingDone = document.createElement("h3");
+  headingDone.innerText = "Done";
+  donediv.appendChild(headingDone);
+  projectdiv.appendChild(donediv);
+  displayTask(project.taskList.Done, donediv);
 }
 
-function displayTask(taskList) {
+function displayTask(taskList, parentdiv) {
   taskList.forEach((task) => {
+    const taskdiv = document.createElement("div");
+    taskdiv.classList.add("task");
+
     const title = document.createElement("h3");
     title.innerText = task.title;
 
@@ -15,8 +37,9 @@ function displayTask(taskList) {
     const dueDate = document.createElement("p");
     dueDate.innerText = task.dueDate;
 
-    projectdiv.appendChild(title);
-    projectdiv.appendChild(description);
-    projectdiv.appendChild(dueDate);
+    taskdiv.appendChild(title);
+    taskdiv.appendChild(description);
+    taskdiv.appendChild(dueDate);
+    parentdiv.appendChild(taskdiv);
   });
 }
