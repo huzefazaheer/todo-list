e4d9075cc2ddb5f2dc7ef08afd83c6eac77c3227
Huzefa <huzefazaheer1+git@gmail.com>
2024-03-25 19:28:22 +0500

    Added class of tasks

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { task } = require("./util/tasks");
+
+tasks = [
+  new task("Eat", "Get fat", "Today"),
+  new task("Sleep", "Zzzzzz", "Today"),
+  new task("Grind", "They don't know me son", "Today"),
+  new task("Eat", "In my mind, I am always the best", "Today"),
+];
---
+++ src/util/tasks.js
@@ -0,0 +1,8 @@
+export class task {
+  constructor(title, description, dueDate, state) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.state = "ToDo";
+  }
+}
